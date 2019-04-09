e8e9ada5e3c076f990f40979179bd991bacb414c
yerzh <ysalkynbayev@gmail.com>
2019-04-09 23:48:18 +0600

    First commit ever

---
+++ Desktop/Projects/Etudes/CellularAutomataTest/UnitTest1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+
+namespace CellularAutomataTest
+{
+    class Game
+    {
+        private int width;
+        private int height;
+        public Game(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public bool[,] NextGen(bool[,] mat)
+        {
+            bool[,] newMat = new bool[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    int count = Neighbours(mat, i, j);
+                    if (count > 3 || count < 2) newMat[i, j] = false;
+                    else if (count == 3) newMat[i, j] = true;
+                    else if (count == 2 && mat[i, j]) newMat[i, j] = true;
+                }
+            }
+            return newMat;
+        }
+
+        public int Neighbours(bool[,] mat, int i, int j)
+        {
+            int count = 0;
+            if (i - 1 >= 0 && j - 1 >= 0 && mat[i - 1, j - 1]) count++;
+            if (i - 1 >= 0 && mat[i - 1, j]) count++;
+            if (i - 1 >= 0 && j + 1 < width && mat[i - 1, j + 1]) count++;
+            if (j - 1 >= 0 && mat[i, j - 1]) count++;
+            if (j + 1 < width && mat[i, j + 1]) count++;
+            if (i + 1 < height && j - 1 >= 0 && mat[i + 1, j - 1]) count++;
+            if (i + 1 < height && mat[i + 1, j]) count++;
+            if (i + 1 < height && j + 1 < width && mat[i + 1, j + 1]) count++;
+
+            return count;
+        }
+    }
+
+    [TestClass]
+    public class GameOfLifeTest
+    {
+        [TestMethod]
+        public void Generation_1_Test()
+        {
+            bool[,] mat = new bool[3, 3];
+            mat[1, 1] = mat[2, 2] = true;
+            Game game = new Game(3, 3);
+            bool[,] answer = new bool[3, 3];
+            answer[1, 2] = answer[2, 1] = true;
+            bool[,] next = game.NextGen(mat);
+            Assert.IsTrue(IsEqual(answer, next));
+        }
+
+        [TestMethod]
+        public void Generation_2_Test()
+        {
+            bool[,] mat = new bool[3, 3];
+            mat[1, 0] = mat[1, 1] = mat[2, 1] = mat[2, 2] = true;
+            Game game = new Game(3, 3);
+            bool[,] answer = new bool[3, 3];
+            answer[1, 0] = answer[1, 1] = 
+            answer[2, 1] = answer[2, 2] = 
+            answer[2, 0] = answer[1, 2] = true;
+            bool[,] next = game.NextGen(mat);
+            Assert.IsTrue(IsEqual(answer, next));
+        }
+
+        [TestMethod]
+        public void Generation_3_Test()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Generation_4_Test()
+        {
+            
+        }
+
+        public bool IsEqual(bool[,] mat1, bool[,] mat2)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (mat1[i, j] != mat2[i, j]) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Desktop/Projects/Etudes/MapColors/ColorGraph.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace MapColors
+{
+    public class ColorGraph
+    {
+        private Dictionary<string, int> dict;
+        private string[] keys;
+        private List<int>[] adj;
+        private int[] colors;
+
+        public int V { get; private set; }
+        public int E { get; private set; }
+
+        public ColorGraph(string filePath)
+        {            
+            dict = new Dictionary<string, int>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (var line in lines)
+            {
+                string[] a = line.Split();
+                foreach(var i in a)
+                    if (!Contains(i))
+                        dict.Add(i, dict.Count);
+            }
+
+            keys = new string[dict.Count];
+            foreach (string name in dict.Keys)
+                keys[dict[name]] = name;
+
+            this.V = keys.Length;
+            this.E = lines.Length;
+            
+            adj = new List<int>[V];
+            for (int i = 0; i < V; i++)
+                adj[i] = new List<int>();
+
+            foreach(var line in lines)
+            {
+                string[] splitted = line.Split();
+                AddEdge(Index(splitted[0]), Index(splitted[1]));
+            }
+        }
+
+        public void AddEdge(int v, int w)
+        {
+            adj[v].Add(w);
+            adj[w].Add(v);
+        }
+
+        public IEnumerable<int> Adj(int v)
+        {
+            return adj[v];
+        }
+
+        public override string ToString()
+        {
+            string s = V + " вершин, " + E + " ребер" + Environment.NewLine;
+            for (int v = 0; v < V; v++)
+            {
+                s += Name(v) + ": ";
+                foreach (int w in this.Adj(v))
+                    s += Name(w) + " ";
+
+                s += Environment.NewLine;
+            }
+
+            return s;
+        }
+
+        public bool Contains(string s) { return dict.ContainsKey(s); }
+
+        public int Index(string s) { return dict[s]; }
+
+        public string Name(int v) { return keys[v]; }
+
+        public bool ColorVertices(int colorCount)
+        {
+            colors = new int[V];
+
+            if (!ColoringUtil(colorCount, 0))
+                return false;
+
+            return true;
+        }
+
+        private bool ColoringUtil(int colorCount, int v)
+        {
+            if (v == V)
+                return true;
+
+            for (int c = 1; c <= colorCount; c++)
+            {
+                if (IsRightColor(v, c))
+                {
+                    colors[v] = c;
+
+                    if (ColoringUtil(colorCount, v + 1))
+                        return true;
+
+                    colors[v] = 0;
+                }
+            }
+
+            return false;
+        }
+
+        private bool IsRightColor(int v, int c)
+        {
+            foreach (var i in Adj(v))
+                if (c == colors[i])
+                    return false;
+
+            return true;
+        }
+
+        public void ShowColors()
+        {
+            Console.WriteLine("Solution Exists: Following are the assigned colors");
+            foreach (var c in colors)
+                Console.Write(c + " ");
+            Console.WriteLine();
+        }
+    }
+}
+
