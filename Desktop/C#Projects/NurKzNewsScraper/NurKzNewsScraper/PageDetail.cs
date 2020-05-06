542168537bea7fac1863407861017687c4091435
Yerzh <ysalkynbayev@gmail.com>
2020-05-06 10:05:07 +0600

    commit

---
+++ Desktop/C#Projects/NurKzNewsScraper/NurKzNewsScraper/PageDetail.cs
@@ -0,0 +1,11 @@
+﻿namespace NurKzNewsScraper
+{
+    public class PageDetail
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+    }
+}
