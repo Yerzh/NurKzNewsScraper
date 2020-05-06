a2bd0fd22644e917a2f867aa3845d694a612c96d
Yerzh <ysalkynbayev@gmail.com>
2020-05-06 10:00:03 +0600

    commit

---
+++ Desktop/C#Projects/Etudes/CellularAutomataTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CellularAutomataTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CellularAutomataTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a7c72e8b-a1aa-45fd-a866-89b83690508d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Desktop/C#Projects/Etudes/MapColors/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace MapColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var filePath = Path.Combine(Environment.CurrentDirectory, @"Res\", "boundaries.txt");
+            ColorGraph cg = new ColorGraph(filePath);
+            if (!cg.ColorVertices(4))
+                Console.WriteLine("Solution doesn't exist");
+            else
+                cg.ShowColors();
+        }
+    }
+}
